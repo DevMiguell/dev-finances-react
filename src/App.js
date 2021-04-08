923c4f9c5eeefb54d1c38fe695ad453d7be8c2cc
DevSP <contatodevsp@gmail.com>
2021-04-08 16:06:47 -0300

    Criando o estilo o Global

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './Pages/Home'
+import { Global } from './Styles/Global'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Global />
+    </>
+  )
+}
+
+export default App
---
+++ src/Styles/Global.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const Global = createGlobalStyle`
+  :root {
+    --dark-blue: #363f5f;
+    --green: #49aa26;
+    --light-green: #3dd705;
+    --red: #e92929;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #f0f2f5;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  html {
+    font-size: 93.75%;
+  }
+`
